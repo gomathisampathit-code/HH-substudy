c7edd566effb4c8a4ff8c1bbfe90939ec7ffd9f3
gomathisampathit-code <gomathisampath.it@gmail.com>
2026-09-12 12:59:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -105,6 +105,7 @@ df_today["display_name"] = df_today.apply(get_display_name, axis=1)
 
 st.write(df.columns.tolist())
 st.write(df_today[["member_id", "episode1"]] if "episode1" in df_today.columns else "column missing")
+st.write(df.columns.tolist())
 
 # Final table
 table = pd.DataFrame({
