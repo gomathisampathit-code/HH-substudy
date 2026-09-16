88ab1834a614951ebc17c8b8d2917306cf0e2079
gomathisampathit-code <gomathisampath.it@gmail.com>
2026-09-16 09:17:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -61,7 +61,8 @@ df["sample_collected"] = (
     .str.lower()
 )
 
-today_str = pd.Timestamp.today().strftime("%Y-%m-%d")
+#today_str = pd.Timestamp.today().strftime("%Y-%m-%d")
+today_str = "2026-09-15"
 
 df_today = df[
     (df["submissiondate"].dt.strftime("%Y-%m-%d") == today_str)
